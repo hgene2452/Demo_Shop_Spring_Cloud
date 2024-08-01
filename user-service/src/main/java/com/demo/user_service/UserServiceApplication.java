9bae8e0e89166e2a8726a0396115878805215245
hgene2452 <hgene0929@gmail.com>
2024-08-02 06:51:28 +0900

    :sparkles: init prj

---
+++ user-service/src/main/java/com/demo/user_service/UserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.demo.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient // 유레카 서버에 등록
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}

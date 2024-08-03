8e19e73fc83839a4720556863425833a7a782cd3
hgene2452 <hgene0929@gmail.com>
2024-08-04 06:04:06 +0900

    :sparkles: Refactor & Add user-apis

--- user-service/src/main/java/com/demo/user_service/data/dto/LoginDto.java
+++
@@ -1,9 +0,0 @@
-package com.demo.user_service.data.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginDto {
-	private String email;
-	private String password;
-}
--- user-service/src/main/java/com/demo/user_service/data/dto/OrderDto.java
+++
@@ -1,7 +0,0 @@
-package com.demo.user_service.data.dto;
-
-import lombok.Data;
-
-@Data
-public class OrderDto {
-}

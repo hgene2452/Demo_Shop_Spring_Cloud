3354f43de62fd80433aeda88cde68c75b0c98832
hgene2452 <hgene0929@gmail.com>
2024-08-10 09:59:43 +0900

    :sparkles: Add LoadBalanced

--- user-service/src/main/java/com/demo/user_service/global/config/RestTemplateConfig.java
+++ user-service/src/main/java/com/demo/user_service/global/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.demo.user_service.global.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
 	@Bean
+	@LoadBalanced // 다른 서비스의 ip를 직접 명시하지 않고 이름을 사용하기 위함
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}

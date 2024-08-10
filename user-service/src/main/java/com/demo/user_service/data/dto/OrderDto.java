f998b09c6cd52c6e605d0e02e78d6a7042a8a519
hgene2452 <hgene0929@gmail.com>
2024-08-10 09:30:36 +0900

    :sparkles: Add restTemplate commnication feature

--- user-service/src/main/java/com/demo/user_service/data/dto/OrderDto.java
+++ user-service/src/main/java/com/demo/user_service/data/dto/OrderDto.java
@@ -4,4 +4,20 @@ import lombok.Data;
 
 @Data
 public class OrderDto {
+	private String productId;
+	private Integer qty;
+	private Integer unitPrice;
+	private Integer totalPrice;
+	private String orderId;
+	private String userId;
+
+	public OrderDto(String productId, Integer qty, Integer unitPrice, Integer totalPrice, String orderId,
+		String userId) {
+		this.productId = productId;
+		this.qty = qty;
+		this.unitPrice = unitPrice;
+		this.totalPrice = totalPrice;
+		this.orderId = orderId;
+		this.userId = userId;
+	}
 }
--- user-service/src/main/java/com/demo/user_service/service/UserServiceImpl.java
+++ user-service/src/main/java/com/demo/user_service/service/UserServiceImpl.java
@@ -4,14 +4,20 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.demo.user_service.auth.JwtTokenProvider;
 import com.demo.user_service.data.dto.LoginDto;
+import com.demo.user_service.data.dto.OrderDto;
 import com.demo.user_service.data.dto.TokenDto;
 import com.demo.user_service.data.dto.UserDto;
 import com.demo.user_service.data.entity.User;
@@ -29,6 +35,8 @@ public class UserServiceImpl implements UserService {
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
 	private final JwtTokenProvider jwtTokenProvider;
+	private final Environment env;
+	private final RestTemplate restTemplate;
 
 	@Override
 	public void createUser(UserDto userDto) {
@@ -45,7 +53,16 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public UserDto findUser(long userId) {
 		UserDto userDto = userRepository.findUserDtoById(userId).orElseThrow(() -> new NotFoundException("UserServiceImpl.findUser", ErrorCode.UNDEFINED_USER));
-		// userDto.setOrderDtos();
+
+		// restTemplate 방식을 통해 order-service로부터 데이터 받아오기 시작
+		String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+		ResponseEntity<List<OrderDto>> orderDtoResp = restTemplate.exchange(
+			orderUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<OrderDto>>() {}
+		);
+		List<OrderDto> orderDtoList = orderDtoResp.getBody();
+		userDto.setOrderDtos(orderDtoList);
+		// restTemplate 방식을 통해 order-service로부터 데이터 받아오기 끝
+
 		return userDto;
 	}
 
